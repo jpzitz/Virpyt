ee1b88edd8c6d31737ec49004519b34205e2e002
Parke <136281346+jpzitz@users.noreply.github.com>
2023-06-16 16:11:23 -0700

    Update VirPyT.py

--- VirPyT.py
+++ VirPyT.py
@@ -1,39 +1,61 @@
-#import openpyxl
-from openpyxl import Workbook
-#wb = Workbook('VirPyT-tester.xlsx')
-
-
-
 import openpyxl
-wb = openpyxl.load_workbook('sample.xlsx')
-ws = wb.active
+from openpyxl import Workbook as book
+        
 
-#type(wb)
+# workbook class with pointer to the workbook
+class Workbook():
+    def __init__():
+        file = openpyxl.workbook()
+            
+    def __init__(self, file):
+        self.file = file
+            
+    @property
+    def file(self):
+        """The file property."""
+        print("Get file")
+        return self._file
+
+    @file.setter
+    def file(self, filename):
+        print("Set filename")
+        self._file = filename
+
+        
+    def sheets(self):
+        return [Sheet.temp_sheet for temp_sheet in self._wb.worksheets] 
 
 
-#    grab the active worksheet
-#ws = wb.active
+# sheet class with pointers to sheets in the workbook        
+class Sheet():
+    def __init__(self, sheet):
+        self._sheet = [openpyxl.workbook.sheetnames]
+        
 
-if ws:
-    print("ok!")
 
 
-s = input()
+        
+#class Table():
+#class Row():
+#class Cell():
+      
+#worksheet = workbook.active
+#print(wb.sheetnames)
+        
 
-if s:
-    #print(ws['A1'])
+if __name__ == '__main__':
+    filename = input(print("Input filename: "))
+    
+    wb = openpyxl.load_workbook(filename)
+    
 
-    # Data can be assigned directly to cells
-    ws['A8'] = 43
+    workbook = Workbook()
+    print(workbook._file)
 
-    # Rows can also be appended
-    #ws.append([1, 2, 3])
+    worksheet = wb.active
+    if worksheet:
+        print("ok!")
 
-    # Python types will automatically be converted
-    import datetime
-    ws['B8'] = datetime.datetime.now()
+    for sheet in workbook.sheets:
+        print("Found sheet named %s" %sheet.title)
 
-    # Save the file
-    wb.save("sample.xlsx")
-else:
-    print(ws['A1'].value)   #test readable
