72853fbaed06fd68cdb2dd4f492acbf6a867340f
Parke <136281346+jpzitz@users.noreply.github.com>
2023-06-16 16:11:53 -0700

    Update virpyt_test02.py

--- virpyt_test02.py
+++ virpyt_test02.py
@@ -1,55 +1,11 @@
 import openpyxl
-#from openpyxl import Workbook
-
-
-
-
-
-
-
-wb = openpyxl.load_workbook(input("Input filename: "))
-
-# workbook class with pointer to the workbook
-class VirPyTWorkbook:
-    def __init__(self, file):
-        self._file = file
-        
-    @property
-    def file(self):
-        """The file property."""
-        print("Get file")
-        return self._file
-
-    @file.setter
-    def file(self, filename):
-        print("Set filename")
-        self._file = filename
-
-    @file.deleter
-    def file(self):
-        print("Clear filename")
-        del self._file
-    
-    sheets = []
-
-
-
-# sheet class with pointers to sheets in the workbook        
-#class VirPyTSheet():
+import VirPyT
+from VirPyT import Workbook, Sheet
     
 
-    
-#class VirPyTTable():
-#class VirPyTRow():
-#class VirPyTCell():
-  
-#worksheet = workbook.active
-#print(wb.sheetnames)
 
-    
-
-workbook = VirPyTWorkbook(_wb)
-print(workbook._file)
+workbook = VirPyT.Workbook('sample.xlsx')
+print(workbook.file)
 
 worksheet = wb.active
 if worksheet:
